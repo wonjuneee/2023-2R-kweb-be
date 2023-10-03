a484e32a585fd8f81e627d44bad417794f2202c0
wonjuneee <tony000709@gmail.com>
2023-10-03 14:53:39 +0900

    Update files

--- Chapter2/circular-shapes.js
+++ Chapter2/circular-shapes.js
@@ -1,13 +1,18 @@
 // Code 2.11 Creating Module (circular-shapes.js)
 const PI = 3.14159265358;
+
 const round = number => Math.round(number * 100) / 100;
+
 const getCircumference = radius => round(2 * PI * radius);
+
 const getCircleArea = radius => round(PI * radius ** 2);
+
 const getCylinderSurfaceArea = (radius, height) => {
-const circleArea = getCircleArea(radius);
-const sideArea = getCircumference(radius) * height;
-return round(2 * circleArea + sideArea);
+    const circleArea = getCircleArea(radius);
+    const sideArea = getCircumference(radius) * height;
+    return round(2 * circleArea + sideArea);
 };
+
 module.exports = {
 getCircumference: getCircumference,
 getCircleArea: getCircleArea,
--- Chapter2/index.js
+++ Chapter2/index.js
@@ -3,6 +3,7 @@ const { getCircumference } = require("./circular-shapes");
 // 비구조화 할당을 통해 모듈 내의 특정 함수만 불러올 수도 있다.
 // getCircumference 라는 이름을 가진 속성의 value를 import.
 const path = require('path');
+// 내부 모듈의 경우 경로 대신 모듈 이름을 입력
 
 const r = 10; 
 
